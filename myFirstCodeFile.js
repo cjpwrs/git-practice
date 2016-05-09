e34441fb3040ae372b934c3783e3a6011dbe1ca8
CJ Powers <cjpowers@CJs-MacBook-Pro.local>
2016-05-09 14:33:27 -0600

    Added 2 new variables to array

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -1,5 +1,9 @@
 var dm11 = "Started today";
+var firstDay = "This is my first day";
+var gitPractice = "This is for practice";
 
 var journal = [];
 
 journal.push(dm11);
+journal.push(firstDay);
+journal.push(gitPractice);
