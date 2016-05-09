4fa630e61b8db06c5210ae771bd4bbefe0d804e8
CJ Powers <cjpowers@CJs-MacBook-Pro.local>
2016-05-09 14:34:26 -0600

    Added one more variable

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -1,9 +1,11 @@
 var dm11 = "Started today";
 var firstDay = "This is my first day";
 var gitPractice = "This is for practice";
+var oneMore = "One more variable";
 
 var journal = [];
 
 journal.push(dm11);
 journal.push(firstDay);
 journal.push(gitPractice);
+journal.push(oneMore);
